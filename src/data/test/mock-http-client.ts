2b7ba7a744ac823c990a176c372061d96d259848
DK <dreik2311@gmail.com>
2025-04-24 12:01:19 -0300

    test: ensure RemoteAuthentication calls httpPostClient with body

---
+++ src/data/test/mock-http-client.ts
@@ -0,0 +1,14 @@
+import {
+  HttpPostClient,
+  httpPostParams,
+} from "../protocols/http/http-post-client";
+
+export class HttpPostClientSpy implements HttpPostClient {
+  url?: string;
+  body?: object;
+
+  async post(params: httpPostParams): Promise<void> {
+    this.url = params.url;
+    this.body = params.body;
+  }
+}
