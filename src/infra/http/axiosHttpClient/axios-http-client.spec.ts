867e78d433a79ea4c5e8bb3e8c62eab239f40081
DK <dreik2311@gmail.com>
2025-04-28 14:59:30 -0300

    test: ensure axios httpClient calls axios with correct url

---
+++ src/infra/http/axiosHttpClient/axios-http-client.spec.ts
@@ -0,0 +1,15 @@
+import { faker } from "@faker-js/faker/.";
+import { AxiosHttpClient } from "./axios-http-client";
+import axios from "axios";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+const url = faker.internet.url();
+
+describe("AxiosHttpClient", () => {
+  test("Should call axios with correct URL", async () => {
+    const sut = new AxiosHttpClient();
+    sut.post({ url: url });
+    expect(mockedAxios).toHaveBeenCalledWith(url);
+  });
+});
